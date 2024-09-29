d08cf66b0832fc07c3acb0c3ba013c2f98db178f
unknown <you@example.com>
2024-09-29 22:18:18 +0530

    routing functionalities have been added

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import "../styles/flowingBorder.css";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -13,13 +14,15 @@ import {
 import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+  const [roomId, setRoomId] = useState<String>("");
 
-  const id = uuidv4();
-  const roomid: string = id.slice(0, 6);
+  useEffect(() => {
+    const id = uuidv4();
+    setRoomId(id.slice(0, 6));
+  }, []);
 
   return (
     <div className="flex flex-row items-center justify-center h-screen bg-zinc-700">
-      <div> {roomid}</div>
       <Card className="w-[500px] h-[350px]">
         <CardHeader className="mb-10">
           <CardTitle>Welcome to SyncWithMates</CardTitle>
@@ -28,16 +31,17 @@ export default function Home() {
         <CardContent>
           <div className="grid w-full items-center ">
             <div className="flex flex-col gap-10">
-              <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-                Create a Room
-              </Button>
+              <Link href={`/room/${roomId}`} legacyBehavior passHref>
+                <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                  Create a Room
+                </Button>
+              </Link>
               <Link href="/join" legacyBehavior passHref>
                 <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
                   Join a Room
                 </Button>
               </Link>
             </div>
-            
           </div>
         </CardContent>
         <CardFooter className="flex justify-between"></CardFooter>
--- my-app/src/app/join/page.tsx
+++ my-app/src/app/join/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { FormEvent } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,8 +13,21 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Join() {
+  const [roomId, setRoomId] = useState("");
+  const [name, setName] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("hi");
+    event.preventDefault();
+    if (name != "" && roomId != "") {
+      router.push(`/room/${roomId}`);
+    }
+  };
   return (
     <div className="flex flex-row items-center justify-center h-screen bg-zinc-700">
       <Card className="w-[500px] h-[350px]">
@@ -22,7 +37,7 @@ export default function Join() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid w-full items-center gap-2">
               <div className="flex flex-col space-y-1.5 ">
                 <Label className="text-lg" htmlFor="name">
@@ -32,6 +47,9 @@ export default function Join() {
                   className="h-11 text-md"
                   id="name"
                   placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
                 />
               </div>
               <div className="flex flex-col space-y-1.5">
@@ -42,21 +60,30 @@ export default function Join() {
                   className="h-11 text-md"
                   id="roomid"
                   placeholder="Enter the room id"
+                  value={roomId}
+                  onChange={(e) => {
+                    setRoomId(e.target.value);
+                  }}
+                  required
                 />
               </div>
             </div>
+            <div className="flex justify-between flex items-center p-6 pt-9">
+              <Link href="/home" legacyBehavior passHref>
+                <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                  Back
+                </Button>
+              </Link>
+
+              <Button
+                type="submit"
+                className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              >
+                Enter room
+              </Button>
+            </div>
           </form>
         </CardContent>
-        <CardFooter className="flex justify-between">
-          <Link href="/home" legacyBehavior passHref>
-            <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-              Back
-            </Button>
-          </Link>
-          <Button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-            Enter room
-          </Button>
-        </CardFooter>
       </Card>
     </div>
   );
